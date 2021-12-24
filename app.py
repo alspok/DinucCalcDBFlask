be59311bbdd164c4552829fcc92c12b53fb0d38e
alvydas spokauskas <alspok@gmail.com>
2021-12-24 13:29:49 +0200

    .

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def hello_world():
+def main():
 #   return "Hello World, hello evrybody and evryone!"
-    return render_template("base.html")
+    with open("access_id", "r") as fh:
+        file_lines = fh.readlines()
+
+    return render_template("main.html", file_lines = file_lines)
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host="127.0.0.1", port=5000, debug=True)
